4ed52e6204e9ce12cf6a7530f16846bfc170e249
Vitor Sorpile <vitorsorpile@gmail.com>
2020-07-03 15:28:49 -0300

    Added filter/order/search bar

--- errors/views.py
+++ errors/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from rest_framework import viewsets
 from errors.models import Error 
 from errors.forms import ErrorForm
+from django.db.models.query import QuerySet
 
 from errors.api.serializers import ErrorSerializer
 from rest_framework.authtoken.models import Token
@@ -12,16 +13,49 @@ class ErrorApiViewSet(viewsets.ModelViewSet):
     queryset = Error.objects.all()
     serializer_class = ErrorSerializer
 
+
 def index_view(request):
     user = request.user
-
     if not user.is_authenticated:
         return redirect('login')
+    
     context = {}
     
     token = Token.objects.get(user = user.pk)
     context['token'] = token
-    errors = Error.objects.filter(user = user.pk, archived=False)
+
+    context['categories'] = [('', 'Selecionar categoria'), ('PRODUÇÃO', 'Produção'),
+                             ('HOMOLOGAÇÃO', 'Homologação'), ('DEV', 'Dev')
+                            ]
+    selectedCategory = None
+    
+    context['order'] = [('', 'Ordenar por'), ('level', 'Level'),
+                        ('date', 'Mais antigo'), ('-date', 'Mais recente')
+                       ]
+    selectedOrder = None
+    
+    context['search'] = [('', 'Buscar por'), ('title', 'Título'),
+                         ('description', 'Descrição'), ('address', 'Origem')
+                        ]
+    selectedSearch = None
+
+    query = ''
+    if request.GET:
+        selectedCategory = request.GET['category']  
+        selectedOrder = request.GET['orderBy']
+        selectedSearch = request.GET['searchBy']
+        query = request.GET['query']
+
+    context['selectedCategory'] = selectedCategory
+    context['selectedOrder'] = selectedOrder
+    context['selectedSearch'] = selectedSearch
+    context['query'] = query
+
+    errors = get_errors_by_category(selectedCategory).filter(user=user.pk, archived=False)
+    errors = get_error_queryset(errors, query, selectedSearch)
+    
+    if selectedOrder:
+        errors = errors.order_by(selectedOrder)
 
     context['errors'] = errors
 
@@ -81,10 +115,11 @@ def archive_error_view(request, error_id):
 
     error = Error.objects.get(pk = error_id)
     if user == error.user:
-        error.archived = True
+        error.archived = not error.archived
         error.save()
     
     return redirect('index')
+
 
 def detail_error_view(request, error_id):
     user = request.user
@@ -97,8 +132,8 @@ def detail_error_view(request, error_id):
         return redirect('login')
 
     error = Error.objects.get(pk = error_id)
-    # if user != error.user:
-    #     return redirect('index')
+    if user != error.user:
+        return redirect('index')
     
     context['error'] = error
     errorUser = Token.objects.get(user = error.user.pk)
@@ -114,9 +149,64 @@ def archived_errors_view(request):
 
     context = {}
 
-    token = Token.objects.get(user = user.pk)
+    token = Token.objects.get(user=user.pk)
     context['token'] = token
     
-    errors = Error.objects.filter(archived=True)
+    context['categories'] = [('', 'Selecionar categoria'), ('PRODUÇÃO', 'Produção'),
+                             ('HOMOLOGAÇÃO', 'Homologação'), ('DEV', 'Dev')
+                            ]
+    selectedCategory = None
+    
+    context['order'] = [('', 'Ordenar por'), ('level', 'Level'),
+                        ('date', 'Mais antigo'), ('-date', 'Mais recente')
+                       ]
+    selectedOrder = None
+    
+    context['search'] = [('', 'Buscar por'), ('title', 'Título'),
+                         ('description', 'Descrição'), ('address', 'Origem')
+                        ]
+    selectedSearch = None
+
+    query = ''
+    if request.GET:
+        selectedCategory = request.GET['category']  
+        selectedOrder = request.GET['orderBy']
+        selectedSearch = request.GET['searchBy']
+        query = request.GET['query']
+
+    context['selectedCategory'] = selectedCategory
+    context['selectedOrder'] = selectedOrder
+    context['selectedSearch'] = selectedSearch
+    context['query'] = query
+
+    errors = get_errors_by_category(selectedCategory).filter(user=user.pk, archived=True)
+    errors = get_error_queryset(errors, query, selectedSearch)
+    
+    if selectedOrder:
+        errors = errors.order_by(selectedOrder)
+
     context['errors'] = errors
     return render(request, 'index.html', context=context)
+
+
+def get_errors_by_category(category=None):
+    if category:
+        return Error.objects.filter(category=category)
+    
+    return Error.objects.all()
+
+
+def get_error_queryset(queryset, query, searchBy=None):
+    if not searchBy:
+        return queryset
+    
+    qs = QuerySet(model=Error).none()
+
+    queries = query.split(' ')
+    search = searchBy + '__icontains'
+
+    for q in queries:
+        errors = queryset.filter(**{search: q})
+        qs = qs.union(errors)
+
+    return qs
